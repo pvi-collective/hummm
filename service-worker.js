c876bf2590b7d209bf36c9a810f9bbaa22d66b13
pvi-collective <projects@pvicollective.com>
2026-08-31 18:59:39 +1000

    Refresh haptic field assets instead of serving stale cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
-const CACHE = 'hummm-build-001-v3';
+const CACHE = 'hummm-field-v0.6.2';
 const FILES = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
